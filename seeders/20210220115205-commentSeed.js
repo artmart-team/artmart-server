8c1ac139bbab0566d3a29789d223440f1522a3bd
Muamar Al Farabi <muamaralfarabi@gmail.com>
2021-02-24 00:43:49 +0700

    Tinggal SeedingOrder

--- seeders/20210220115205-commentSeed.js
+++ seeders/20210220115205-commentSeed.js
@@ -5,23 +5,37 @@ module.exports = {
 
     const comment = [
       {
-        description : "buat test delete comment",
-        UserId : 1,
+        description : "I would like to have some too!",
+        UserId : 4,
         ArtistId : 1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        description : "buat test edit comment",
-        UserId : 1,
+        description : "Can you do this style for me tho? Looks good!",
+        UserId : 3,
         ArtistId : 1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        description : "buat test get comment by id",
-        UserId : 1,
-        ArtistId : 1,
+        description : "Incredibly minimalist, simple but deep! I like it, boss!",
+        UserId : 3,
+        ArtistId : 6,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        description : "This, gentlement, is what I called a magnum opus! Love it ❤ !",
+        UserId : 5,
+        ArtistId : 3,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        description : "Dali never fail to amuse me! 👍👍",
+        UserId : 5,
+        ArtistId : 2,
         createdAt: new Date(),
         updatedAt: new Date()
       }
--- seeders/20210220115308-ratingSeed.js
+++ seeders/20210220115308-ratingSeed.js
@@ -6,35 +6,91 @@ module.exports = {
 
     const rating = [
       {
-        score : 2,
-        ArtistId : 1,
-        UserId : 1,
+        score: 4.5,
+        ArtistId: 1,
+        UserId: 1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        score : 2,
-        ArtistId : 1,
-        UserId : 1,
+        score: 5,
+        ArtistId: 1,
+        UserId: 2,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        score : 3,
-        ArtistId : 2,
-        UserId : 1,
+        score: 4,
+        ArtistId: 2,
+        UserId: 1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        score : 4,
-        ArtistId : 1,
-        UserId : 1,
+        score: 5,
+        ArtistId: 2,
+        UserId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        score: 4.5,
+        ArtistId: 3,
+        UserId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        score: 4.5,
+        ArtistId: 3,
+        UserId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        score: 5,
+        ArtistId: 4,
+        UserId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        score: 3,
+        ArtistId: 4,
+        UserId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        score: 4,
+        ArtistId: 5,
+        UserId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        score: 4,
+        ArtistId: 5,
+        UserId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        score: 5,
+        ArtistId: 6,
+        UserId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        score: 5,
+        ArtistId: 6,
+        UserId: 2,
         createdAt: new Date(),
         updatedAt: new Date()
       }
     ]
-   await queryInterface.bulkInsert('Ratings', rating)
+    await queryInterface.bulkInsert('Ratings', rating)
   },
 
   down: async (queryInterface, Sequelize) => {
